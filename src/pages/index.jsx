47b963419153c76184e383bf6d12694323978420
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-05-02 01:22:23 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,26 @@
 import Head from 'next/head';
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Footer } from 'src/components/Footer';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
 import styles from 'src/styles/Home.module.css';
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  },[]);
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     // マウント時の処理
     document.body.style.backgroundColor = 'lightblue';
-    console.log('マウント時');
     // アンマウント時の処理
     return () => {
       document.body.style.backgroundColor = '';
-      console.log('アンマウント時');
     };
   }, []);
-
 
   return (
     <div className={styles.container}>
@@ -32,7 +28,8 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <a href='/about' onClick={handleClick}>ボタン</a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page='index' />
       <Footer />
     </div>
