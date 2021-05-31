a7ddc1dae987413a3da71b9f52f7883dc8e0014a
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-06-01 01:50:45 +0900

    ⚰️ 余分なconsole.logを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,6 @@ import { Main } from 'src/components/Main';
 import styles from 'src/styles/Home.module.css';
 
 export default function Home(props) {
-  console.log(props);
   const {
     count,
     isShow,
