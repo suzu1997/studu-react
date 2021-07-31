10ffd6f8820393d4f6069135d758c3bd68409471
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-31 11:08:21 +0900

    🔥 Indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,41 +1,18 @@
 import Head from 'next/head';
-import { Footer } from 'src/components/Footer';
 import { Header } from 'src/components/Header';
-import { Main } from 'src/components/Main';
 import styles from 'src/styles/Home.module.css';
 
-const About = (props) => {
-  const {
-    doubleCount,
-    isShow,
-    handleClick,
-    handleDisplay,
-    text,
-    array,
-    handleChenge,
-    handleAdd,
-  } = props;
-
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Index Page</title>
       </Head>
       <Header />
-      <button onClick={handleClick}>ボタン</button>
-      <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
-      {isShow ? <h2>{doubleCount}</h2> : null}
-      <input type='text' value={text} onChange={handleChenge} />
-      <button onClick={handleAdd}>追加</button>
-      <ul>
-        {array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-      <Main page='about' />
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaceholderのAPIを色々叩いて見るよ！</p>
     </div>
   );
 };
 
-export default About;
+export default Index;
