c19223fa032089d83ac98e1ba15ef3e80ad1914c
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-07-31 11:11:58 +0900

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,13 @@
 import Head from 'next/head';
-import { useBgColor } from 'src/hooks/useBgColor';
-import { useCounter } from 'src/hooks/useCounter';
-import { useInputArray } from 'src/hooks/useInputArray';
 import 'src/styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
-
   return (
     <>
       <Head>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray} />
+      <Component {...pageProps} />
     </>
   );
 };
