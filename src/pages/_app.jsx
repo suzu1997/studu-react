15ae5473ae64a4250d4d78a3f4eebd70d122f749
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-09-10 21:47:01 +0900

    ➕ Tailwind CSS のセットアップ

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Layout } from 'src/components/Layout/Index';
 import 'src/styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
