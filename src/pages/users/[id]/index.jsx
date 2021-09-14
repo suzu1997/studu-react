315eddaa4ad876d85d25286cc05dd6ac4be8aeed
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-09-14 14:06:52 +0900

    ✨ ユーザー詳細ページでSSRを実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,12 +1,30 @@
 import { Header } from 'src/components/Header';
 import { User } from 'src/components/User';
+import { SWRConfig } from 'swr';
 
-const UsersId = () => {
+//getServerSidePropsを使うと、ブラウザ側ではなくサーバー側で動く
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData, // APIを叩くときの初期値をuserDataにする
+      },
+    },
+  };
+};
+
+const UsersId = (props) => { // propsにfallbackが渡ってくる 
+  const { fallback } = props;
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <User />
-    </div>
+    </SWRConfig>
   );
 };
 
