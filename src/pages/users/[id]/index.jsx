1a27953776c0a6284ebad9d0b4e9a73cb8de05ba
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-09-16 14:19:43 +0900

    👽️ ユーザー一覧ページと、ユーザー詳細ページのユーザーの投稿でのSSRの実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -5,20 +5,29 @@ import { SWRConfig } from 'swr';
 //getServerSidePropsを使うと、ブラウザ側ではなくサーバー側で動く
 export const getServerSideProps = async (ctx) => {
   const { id } = ctx.query;
-  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
-  const user = await fetch(API_URL);
+  // ユーザー情報の取得
+  const USER_API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(USER_API_URL);
   const userData = await user.json();
+
+  // ユーザーの投稿の取得
+  const POSTS_API_URL = `https://jsonplaceholder.typicode.com/posts?userId=${userData.id}`;
+  const posts = await fetch(POSTS_API_URL);
+  const postsData = await posts.json();
 
   return {
     props: {
       fallback: {
-        [API_URL]: userData, // APIを叩くときの初期値をuserDataにする
+        // APIを叩くときの初期値をuserDataにする
+        [USER_API_URL]: userData,
+        [POSTS_API_URL]: postsData,
       },
     },
   };
 };
 
-const UsersId = (props) => { // propsにfallbackが渡ってくる 
+const UsersId = (props) => {
+  // propsにfallbackが渡ってくる
   const { fallback } = props;
   return (
     <SWRConfig value={{ fallback }}>
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,15 +1,35 @@
 import Head from 'next/head';
 import { Header } from 'src/components/Header';
 import { Users as UsersComponent } from 'src/components/Users';
+import { SWRConfig } from 'swr';
 
-const Users = () => {
+export const getServerSideProps = async () => {
+  // ユーザー一覧の取得
+  const USERS_API_URL = 'https://jsonplaceholder.typicode.com/users';
+  const users = await fetch(USERS_API_URL);
+  const usersData = await users.json();
+
+  return {
+    props: {
+      fallback: {
+        // APIを叩くときの初期値をuserDataにする
+        [USERS_API_URL]: usersData,
+      },
+    },
+  };
+};
+
+const Users = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Users Page</title>
       </Head>
-      <Header />
-      <UsersComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UsersComponent />
+      </SWRConfig>
     </div>
   );
 };
